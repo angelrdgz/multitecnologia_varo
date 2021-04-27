b4058bd1da8b9673f25f9fd8d419771d0d70e432
Ángel García <angelrodriguez@ucol.mx>
2021-04-26 20:23:53 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Button, Modal, Dialog } from "attractions";
+  import { Router, Route, Link } from "svelte-navigator";
+  import Home from './Components/Home.svelte'
+  import About from './Components/About.svelte'
+
+
+</script>
+
+<Router>
+	<header>
+	  <h1>History</h1>
+  
+	  <nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+		<Link to="profile">Profile</Link>
+	  </nav>
+	</header>
+  
+	<main>
+	  <Route path="/">
+		<Home />
+	  </Route>
+  
+	  <Route path="/about">
+		<About />
+	  </Route>
+	</main>
+  </Router>
